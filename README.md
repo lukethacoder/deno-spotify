907423bff140951cbcfb42e41dacbb7e7b48a3d2
Luke Secomb <luke.secomb@live.com>
2020-09-07 23:57:03 +1000

    🎷 initial commit feat. spotify oauth2 flow

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # deno-spotify
-Messing around with Deno + Spotify API (currently running OAuth2 nicely)
+
+Messing around with [Deno](https://deno.land) + [Spotify API](https://developer.spotify.com/documentation/).
+
+## Install
+
+...none, Welcome to Deno
+
+> What is [Deno](https://deno.land)?
+
+## Run
+
+```bash
+deno run --allow-read --allow-net spotify-auth.ts
+```
+
+### Environment Variables
+
+Make sure to get your Spotify Developer keys and add them to an `.env` file.
+
+> The `.env.example` gives an example of what ENV VARS you'll need.
---
+++ spotify-auth.ts
@@ -0,0 +1,101 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { opine } from "https://deno.land/x/opine@0.21.3/mod.ts";
+import {
+  Request,
+  Response,
+  ResponseBody,
+} from "https://deno.land/x/opine@0.21.3/src/types.ts";
+
+const app = opine();
+const env = config();
+
+const PORT: number = parseInt(env.PORT) || 3000;
+const BASE_API_URL = "https://accounts.spotify.com/api";
+const SERVER_ADDRESS = `http://localhost:${PORT}`;
+const SPOTIFY_CLIENT_ID = env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = env.SPOTIFY_CLIENT_SECRET;
+
+let afterLoginURI = `http://localhost:${PORT}/success`;
+let access_token = "";
+let refresh_token = "";
+let expires_in = "";
+let loginInitiated = false;
+
+let redirect_uri = `${SERVER_ADDRESS}/callback`;
+
+app.get("/", (req, res) => {
+  res.send(
+    `<div style="font-family: monospace;">
+      <h1>Welcome to Deno Spotify OAuth</h1>
+      <p>Please navigate to <a href="/login">/login</a> to continue.</p>
+    </div>`,
+  );
+});
+
+app.get("/login", (req, res) => {
+  let login_url = new URL(`https://accounts.spotify.com/authorize?`);
+
+  const body: URLSearchParams = new URLSearchParams({
+    response_type: "code",
+    client_id: SPOTIFY_CLIENT_ID,
+    scope:
+      "user-read-playback-state user-read-currently-playing user-modify-playback-state user-read-private user-read-email",
+    redirect_uri: redirect_uri,
+  });
+
+  let auth_url_with_params = login_url + body.toString();
+  res.redirect(auth_url_with_params);
+});
+
+app.get("/callback", function (req: Request, res: Response) {
+  let code = req.query.code || null;
+
+  const body: URLSearchParams = new URLSearchParams({
+    client_id: SPOTIFY_CLIENT_ID,
+    client_secret: SPOTIFY_CLIENT_SECRET,
+    code: code,
+    redirect_uri: redirect_uri,
+    grant_type: "authorization_code",
+  });
+
+  fetch(`${BASE_API_URL}/token`, {
+    method: "POST",
+    headers: {
+      contentType: "application/x-www-form-urlencoded",
+    },
+    body: body,
+  })
+    .then((response: any) => response.json())
+    .then((sp_res: any) => {
+      access_token = sp_res.access_token;
+      refresh_token = sp_res.refresh_token;
+      expires_in = sp_res.expires_in;
+
+      loginInitiated = true;
+      res.redirect("/success");
+    })
+    .catch((err: any) => {
+      console.error("err ", err);
+      res.redirect("/failed");
+    });
+});
+
+app.get("/success", function (req: Request, res: Response) {
+  res.send(`
+    <div style="font-family: monospace;">
+      <h1>Successfully OAuthed Spotify</h1>
+      <p>access_token: ${access_token}</p>
+      <p>refresh_token: ${refresh_token}</p>
+      <p>expires_in: ${expires_in}</p>
+    </div>
+  `);
+});
+
+app.get("/failed", function (req: Request, res: Response) {
+  res.send("Failed to OAuth with Spotify");
+});
+
+console.log(
+  `Listening on port ${PORT}. Go to ${SERVER_ADDRESS}/login to initiate authentication flow.`,
+);
+app.listen(PORT);
